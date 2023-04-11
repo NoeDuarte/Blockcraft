63a1fcc880f339901c60215af73e6966a29cf501
NoeDuarte <noeliaduarte.95@gmail.com>
2023-04-11 19:47:04 -0300

    Jugar

---
+++ Arma.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blockcraft
+{
+    internal class Arma
+    {
+        public void picoDiamante()
+        {
+
+        }
+        public void hachaPiedra()
+        {
+
+        }
+        public int espadaOro()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Enemigo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blockcraft
+{
+    internal class Enemigo
+    {
+        private Arma arma;
+        private int vida;
+
+        public Enemigo(Arma arma, int vida)
+        {
+
+            this.arma = arma;
+            this.vida = vida;
+
+        }
+
+        public double ataque(int vida, Arma arma)
+        {
+            double nuevaVida;
+
+            return 0;
+
+
+        }
+    }
+}
